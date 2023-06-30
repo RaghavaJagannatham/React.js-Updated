a934ad73a05e8752b05cc1fa49d6d4f05a9fbf5e
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2023-06-30 10:16:12 +0530

    Updated Counter.jsx
    Added Normal reducer styles

--- react-app/src/Components/Counter.jsx
+++ react-app/src/Components/Counter.jsx
@@ -2,13 +2,16 @@ import React, {useReducer} from 'react'
 import {Button, ButtonGroup} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const initialCount = 0;
+const initialCount = {
+    counter1:0,
+    counter2:10
+}
 const reducer = (state,action) => {
-    switch (action) {
+    switch (action.type) {
         case 'increment':
-            return state+1;
+            return {...state, counter1: state.counter1+1};
         case 'decrement':
-            return state-1;
+            return {...state, counter1: state.counter1-1};
         case 'reset':
             return initialCount;
         default:
@@ -20,15 +23,15 @@ const Counter = () => {
     const[count,setCount] = useReducer(reducer,initialCount);
   return (
     <div>
-        <div>Counter : {count}</div>
+        <div>Counter : {count.counter1}</div>
         <ButtonGroup>
-            <Button color="danger" onClick={() =>setCount('increment')}>
+            <Button color="danger" onClick={() =>setCount({type:'increment'})}>
                 Increment
             </Button>
-            <Button color="warning" onClick={() => setCount('decrement')}>
+            <Button color="warning" onClick={() => setCount({type:'decrement'})}>
                 Decrement
             </Button>
-            <Button color="success" onClick={() => setCount('reset')}>
+            <Button color="success" onClick={() => setCount({type:'reset'})}>
                 Reset
             </Button>
         </ButtonGroup>
