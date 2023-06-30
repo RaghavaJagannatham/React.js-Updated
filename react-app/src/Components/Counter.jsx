ceebfc8d4772d4faeffd51e09670e0c5c03b268d
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2023-06-30 10:26:56 +0530

    Updated Counter.jsx
    Working with Payload

--- react-app/src/Components/Counter.jsx
+++ react-app/src/Components/Counter.jsx
@@ -1,5 +1,5 @@
 import React, {useReducer} from 'react'
-import {Button, ButtonGroup} from 'reactstrap'
+import {Badge, Button, ButtonGroup} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const initialCount = {
@@ -9,9 +9,15 @@ const initialCount = {
 const reducer = (state,action) => {
     switch (action.type) {
         case 'increment':
-            return {...state, counter1: state.counter1+1};
+            return {...state, counter1: state.counter1 + action.payload};
         case 'decrement':
-            return {...state, counter1: state.counter1-1};
+            return {...state, counter1: state.counter1 - action.payload};
+        case 'reset':
+            return initialCount;
+        case 'increment2':
+            return {...state, counter2: state.counter2 + action.payload};
+        case 'decrement2':
+            return {...state, counter2: state.counter2 - action.payload};
         case 'reset':
             return initialCount;
         default:
@@ -23,18 +29,47 @@ const Counter = () => {
     const[count,setCount] = useReducer(reducer,initialCount);
   return (
     <div>
-        <div>Counter : {count.counter1}</div>
         <ButtonGroup>
-            <Button color="danger" onClick={() =>setCount({type:'increment'})}>
-                Increment
-            </Button>
-            <Button color="warning" onClick={() => setCount({type:'decrement'})}>
-                Decrement
-            </Button>
-            <Button color="success" onClick={() => setCount({type:'reset'})}>
-                Reset
+            <Button color='primary' outline>
+                Counter 1 <Badge color='secondary'> {count.counter1} </Badge>
             </Button>
         </ButtonGroup>
+        <ButtonGroup>
+            <Button color='primary' outline>
+                Counter 2 <Badge color='secondary'> {count.counter2} </Badge>
+            </Button>
+        </ButtonGroup>
+        <p></p>
+        <ButtonGroup>
+            <Button color="danger" onClick={() =>setCount({type:'increment',payload:1})}>
+                Increment
+            </Button>
+            <Button color="warning" onClick={() => setCount({type:'decrement',payload:1})}>
+                Decrement
+            </Button>
+        </ButtonGroup>
+        <p></p>
+        <ButtonGroup>
+            <Button color="danger" onClick={() =>setCount({type:'increment', payload : 5})}>
+                Increment Payload
+            </Button>
+            <Button color="warning" onClick={() => setCount({type:'decrement', payload:5})}>
+                Decrement Payload
+            </Button>
+        </ButtonGroup>
+        <p></p>
+        <ButtonGroup>
+            <Button color="danger" onClick={() =>setCount({type:'increment2', payload:10})}>
+                Counter Increment2
+            </Button>
+            <Button color="warning" onClick={() => setCount({type:'decrement2', payload:10})}>
+                Counter Decrement2
+            </Button>
+        </ButtonGroup>
+        <p></p>
+        <Button color="success" onClick={() => setCount({type:'reset'})}>
+                Reset
+        </Button>
     </div>
   )
 }
